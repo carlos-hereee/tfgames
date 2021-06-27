e302121b59a04ce776ea41f7a14e201cf203b3c9
ch04937 <97hernandez.c@gmail.com>
2021-06-26 18:42:05 -0700

    created board for game

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,4 +1,6 @@
-const Room = () => {
-  return <div> X </div>;
+const Room = ({ room }) => {
+  return (
+    <div className={`room x:${room.x} y:${room.y} a:${room.active}`}> X </div>
+  );
 };
 export default Room;
--- src/components/TickTackToe.js
+++ src/components/TickTackToe.js
@@ -1,28 +1,31 @@
 import { useState } from "react";
 import shortid from "shortid";
+import { randomBoolean } from "../utils/math";
 import Room from "./Room";
 
 const TickTackToe = () => {
   const [start, setStart] = useState(false);
   const rooms = [
-    { x: 1, y: 1, id: shortid.generate() },
-    { x: 2, y: 1, id: shortid.generate() },
-    { x: 3, y: 1, id: shortid.generate() },
-    { x: 1, y: 2, id: shortid.generate() },
-    { x: 2, y: 2, id: shortid.generate() },
-    { x: 3, y: 2, id: shortid.generate() },
-    { x: 1, y: 3, id: shortid.generate() },
-    { x: 2, y: 3, id: shortid.generate() },
-    { x: 3, y: 3, id: shortid.generate() },
+    { x: 1, y: 1, id: shortid.generate(), active: randomBoolean() },
+    { x: 2, y: 1, id: shortid.generate(), active: randomBoolean() },
+    { x: 3, y: 1, id: shortid.generate(), active: randomBoolean() },
+    { x: 1, y: 2, id: shortid.generate(), active: randomBoolean() },
+    { x: 2, y: 2, id: shortid.generate(), active: randomBoolean() },
+    { x: 3, y: 2, id: shortid.generate(), active: randomBoolean() },
+    { x: 1, y: 3, id: shortid.generate(), active: randomBoolean() },
+    { x: 2, y: 3, id: shortid.generate(), active: randomBoolean() },
+    { x: 3, y: 3, id: shortid.generate(), active: randomBoolean() },
   ];
   return (
     <div>
       <button type="button" onClick={() => setStart(!start)}>
         {start ? "Play Game" : "Reset Game"}
       </button>
-      {rooms.map((data) => (
-        <Room key={data.id} />
-      ))}
+      <div className="tictactoe">
+        {rooms.map((data) => (
+          <Room key={data.id} room={data} />
+        ))}
+      </div>
     </div>
   );
 };
