c7ed8502dcc893e96cee4358622b7c2442aee682
ch04937 <97hernandez.c@gmail.com>
2021-06-28 16:03:26 -0700

    referees take notes

--- src/components/TickTackToe.js
+++ src/components/TickTackToe.js
@@ -6,27 +6,41 @@ const TickTackToe = () => {
   const [player, setPlayer] = useState("X");
   const [log, setLog] = useState([]);
   const rooms = [
-    { x: 1, y: 1, id: shortid.generate(), content: null },
-    { x: 2, y: 1, id: shortid.generate(), content: null },
-    { x: 3, y: 1, id: shortid.generate(), content: null },
-    { x: 1, y: 2, id: shortid.generate(), content: null },
-    { x: 2, y: 2, id: shortid.generate(), content: null },
-    { x: 3, y: 2, id: shortid.generate(), content: null },
-    { x: 1, y: 3, id: shortid.generate(), content: null },
-    { x: 2, y: 3, id: shortid.generate(), content: null },
-    { x: 3, y: 3, id: shortid.generate(), content: null },
+    { x: 1, y: 1, content: null },
+    { x: 2, y: 1, content: null },
+    { x: 3, y: 1, content: null },
+    { x: 1, y: 2, content: null },
+    { x: 2, y: 2, content: null },
+    { x: 3, y: 2, content: null },
+    { x: 1, y: 3, content: null },
+    { x: 2, y: 3, content: null },
+    { x: 3, y: 3, content: null },
   ];
 
-  // check if you have a win
-  const referee = [
-    { id: 0, notes: [], winner: false },
-    { id: 1, notes: [], winner: false },
-    { id: 2, notes: [], winner: false },
-    { id: 3, notes: [], winner: false },
-    { id: 4, notes: [], winner: false },
-    { id: 5, notes: [], winner: false },
-  ];
+  const [referee, setReferee] = useState([
+    { id: 0, x: 1, notes: [], winner: false },
+    { id: 1, x: 2, notes: [], winner: false },
+    { id: 2, x: 3, notes: [], winner: false },
+    { id: 3, y: 1, notes: [], winner: false },
+    { id: 4, y: 2, notes: [], winner: false },
+    { id: 5, y: 3, notes: [], winner: false },
+  ]);
 
+  const playerMove = (data) => {
+    data.content = player;
+    // add the player move to the refs notes
+    const refereeToUpdate = referee.filter((item) => {
+      return item.x === data.x || item.y === data.y;
+    });
+    refereeToUpdate.forEach((item) => {
+      item.notes.push(player);
+    });
+    setLog([
+      ...log,
+      `Player: ${player} made a move its now ${turnSwap[player]}'s turn`,
+    ]);
+    setPlayer(turnSwap[player]);
+  };
   return (
     <div className="game">
       <div>
@@ -34,15 +48,8 @@ const TickTackToe = () => {
           {rooms.map((data) => (
             <button
               className={`room x-${data.x} y-${data.y}`}
-              key={data.id}
-              onClick={() => {
-                setLog([
-                  ...log,
-                  `Player: ${player} made a move its now ${turnSwap[player]}'s turn`,
-                ]);
-                data.content = player;
-                setPlayer(turnSwap[player]);
-              }}
+              key={shortid.generate()}
+              onClick={() => playerMove(data)}
               disabled={data.content}>
               {data.content}
             </button>
@@ -55,7 +62,7 @@ const TickTackToe = () => {
             <div
               key={data.id}
               className={`referee x-${data.id > 2 ? "2" : "1"}`}>
-              <p>Room: {data.id}</p>
+              <p>Referee: {data.id}</p>
               {data.notes.map((item) => (
                 <p>{item}</p>
               ))}
