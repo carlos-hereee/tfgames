eb1b56226d87135a3a1102319924b74fac8e5cec
ch04937 <97hernandez.c@gmail.com>
2021-06-28 17:15:19 -0700

    winner logic

--- src/components/TickTackToe.js
+++ src/components/TickTackToe.js
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import shortid from "shortid";
+import { allCharactersSame } from "../utils/math";
 
 const TickTackToe = () => {
-  const turnSwap = { X: "O", O: "X" };
   const [player, setPlayer] = useState("X");
+  const [winner, setWinner] = useState(false);
   const [log, setLog] = useState([]);
-  const rooms = [
+  const [rooms, setRooms] = useState([
     { x: 1, y: 1, content: null },
     { x: 2, y: 1, content: null },
     { x: 3, y: 1, content: null },
@@ -15,26 +16,36 @@ const TickTackToe = () => {
     { x: 1, y: 3, content: null },
     { x: 2, y: 3, content: null },
     { x: 3, y: 3, content: null },
-  ];
-
-  const [referee, setReferee] = useState([
-    { id: 0, x: 1, notes: [], winner: false },
-    { id: 1, x: 2, notes: [], winner: false },
-    { id: 2, x: 3, notes: [], winner: false },
-    { id: 3, y: 1, notes: [], winner: false },
-    { id: 4, y: 2, notes: [], winner: false },
-    { id: 5, y: 3, notes: [], winner: false },
   ]);
+  const [referee, setReferee] = useState([
+    { id: 0, x: 1, notes: "" },
+    { id: 1, x: 2, notes: "" },
+    { id: 2, x: 3, notes: "" },
+    { id: 3, y: 1, notes: "" },
+    { id: 4, y: 2, notes: "" },
+    { id: 5, y: 3, notes: "" },
+  ]);
+
+  const turnSwap = { X: "O", O: "X" };
 
   const playerMove = (data) => {
     data.content = player;
     // add the player move to the refs notes
-    const refereeToUpdate = referee.filter((item) => {
+    const refereeNotes = referee.filter((item) => {
+      if (item.x === data.x || item.y === data.y) {
+        item.notes = item.notes += player;
+      }
       return item.x === data.x || item.y === data.y;
     });
-    refereeToUpdate.forEach((item) => {
-      item.notes.push(player);
+    // check for winning move
+    const winner = refereeNotes.filter((i) => {
+      // check for length of 3 characters
+      // check if all characters are the same
+      return i.notes.length > 2 && allCharactersSame(i.notes);
     });
+    if (winner.length > 0) {
+      setWinner(player);
+    }
     setLog([
       ...log,
       `Player: ${player} made a move its now ${turnSwap[player]}'s turn`,
@@ -42,39 +53,40 @@ const TickTackToe = () => {
     setPlayer(turnSwap[player]);
   };
   return (
-    <div className="game">
-      <div>
-        <div className="tictactoe">
-          {rooms.map((data) => (
-            <button
-              className={`room x-${data.x} y-${data.y}`}
-              key={shortid.generate()}
-              onClick={() => playerMove(data)}
-              disabled={data.content}>
-              {data.content}
-            </button>
-          ))}
+    <div>
+      <div className="game">
+        <div>
+          <div className="tictactoe">
+            {rooms.map((data) => (
+              <button
+                className={`room x-${data.x} y-${data.y}`}
+                key={shortid.generate()}
+                onClick={() => playerMove(data)}
+                disabled={data.content}>
+                {data.content}
+              </button>
+            ))}
+          </div>
+        </div>
+        <div className="gamestatus">
+          <div className="status">
+            {referee.map((data) => (
+              <div
+                key={data.id}
+                className={`referee x-${data.id > 2 ? "2" : "1"}`}>
+                <p>Referee: {data.id}</p>
+                <p>{data.notes}</p>
+              </div>
+            ))}
+          </div>
+          <div className="logger">
+            {log.map((data) => (
+              <p key={shortid.generate()}> {data}</p>
+            ))}
+          </div>
         </div>
       </div>
-      <div className="gamestatus">
-        <div className="status">
-          {referee.map((data) => (
-            <div
-              key={data.id}
-              className={`referee x-${data.id > 2 ? "2" : "1"}`}>
-              <p>Referee: {data.id}</p>
-              {data.notes.map((item) => (
-                <p>{item}</p>
-              ))}
-            </div>
-          ))}
-        </div>
-        <div className="logger">
-          {log.map((data) => (
-            <p key={shortid.generate()}> {data}</p>
-          ))}
-        </div>
-      </div>
+      {winner && <div> Winner Winner! </div>}
     </div>
   );
 };
--- src/utils/math.js
+++ src/utils/math.js
@@ -1,1 +1,6 @@
 export const randomBoolean = () => Math.random() >= 0.5;
+export const allCharactersSame = (s) => {
+  let n = s.length;
+  for (let i = 1; i < n; i++) if (s[i] !== s[0]) return false;
+  return true;
+};
