5acb5c0bc6e3c766845b716c1214c5b2d00c34ea
ch04937 <97hernandez.c@gmail.com>
2021-10-05 16:39:38 -0700

    new home page ui

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,14 +1,29 @@
 // import { useContext } from "react";
 // import { PlayerContext } from "../utlils/PlayerContext";
+import shortid from "shortid";
 import GameMenu from "../components/GameMenu";
+import tictactoeGameboard from "../assets/tictactoeGameboard.svg";
 
+const games = [
+  {
+    name: "TicTacToe",
+    key: shortid.generate(),
+    imageName: "tictactoeGameboard",
+  },
+];
 const Homepage = () => {
   // const { player } = useContext(PlayerContext);
+  const image = {
+    tictactoeGameboard,
+  };
   return (
-    <main className="container">
-      {/* <GameMenu /> */}
-
-      <h1>Welcome!</h1>
+    <main className="container homepage">
+      {games.map((game) => (
+        <a href={`/${game.name}`} key={game.key} className="game-card">
+          <h2>{game.name}</h2>
+          <img src={image[game.imageName]} alt="tictactoe board" />
+        </a>
+      ))}
     </main>
   );
 };
