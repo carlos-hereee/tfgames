8d833479cc6c805e6322a9e1106ae7815d76f130
ch04937 <97hernandez.c@gmail.com>
2021-10-21 14:41:27 -0700

    tictactoe styled cells and hover

--- src/components/games/TicTacToe.js
+++ src/components/games/TicTacToe.js
@@ -1,9 +1,22 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { GameContext } from "../../context/GameContext";
+import { PlayerContext } from "../../context/PlayerContext";
 
 const TicTacToe = () => {
   const { game } = useContext(GameContext);
-  console.log(game.board);
+  const { player } = useContext(PlayerContext);
+  const [isPlayer1, setIsPlayer1] = useState(false);
+  console.log(player);
+  console.log(game);
+  useEffect(() => {
+    if (game.turn) {
+      player.uid === game.players.player1.uid
+        ? setIsPlayer1(true)
+        : setIsPlayer1(false);
+    }
+  }, [game.turn]);
+  // player 1 is x
+  // player 2 is o
   return (
     <main className="board tictactoe">
       {game.board.board.length > 1 &&
@@ -11,9 +24,10 @@ const TicTacToe = () => {
           <button
             key={cell.uid}
             disabled={!cell.isEmpty}
-            className={`cell x-${cell.positionX} y-${cell.positionY}`}>
-            {cell.content}
-          </button>
+            className={`cell x-${cell.positionX} y-${cell.positionY} ${
+              game.turn === "player1" && isPlayer1 ? "player1" : "player2"
+            }`}
+          />
         ))}
     </main>
   );
