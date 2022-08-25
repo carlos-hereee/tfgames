0c9d928fed240689241353599ca1ea0def4cb5a1
ch04937 <97hernandez.c@gmail.com>
2022-08-25 16:28:51 -0700

    correct file name

--- src/App.js
+++ src/App.js
@@ -20,8 +20,8 @@ function App() {
   }
   return (
     <div className="app">
-      {/* <Header /> */}
-      {/* <OnlineNav /> */}
+      <Header />
+      <OnlineNav />
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route path="/login" component={Auth} />
