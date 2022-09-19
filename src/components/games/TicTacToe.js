926b7107e38581bdcf352f7b7f6c0234f94282d2
ch04937 <97hernandez.c@gmail.com>
2022-09-19 11:52:41 -0700

    neat: adding game assets to make DRY code

--- src/components/games/TicTacToe.js
+++ src/components/games/TicTacToe.js
@@ -5,15 +5,21 @@ import { AuthContext } from "../../context/AuthContext";
 const TicTacToe = () => {
   const { game, gameUpdate, gameResult } = useContext(GameContext);
   const { player } = useContext(AuthContext);
-  let isPlayer1 = player.uid === game.players?.player1?.uid;
-  let isPlayer2 = player.uid === game.players?.player2?.uid;
+  let isPlayer1 = player.uid === game.player1?.uid;
+  let isPlayer2 = player.uid === game.player2?.uid;
 
   const checkLegalMove = (cell) => {
+    console.log("cell", cell);
     if (!gameResult.result) {
-      if (cell.hasContent && game.turn === "player1" && isPlayer1) {
+      console.log("game result", gameResult);
+      if (!cell.hasContent && game.turn === "player1" && isPlayer1) {
+        console.log(
+          "cell has content",
+          !cell.hasContent && game.turn === "player1" && isPlayer1
+        );
         gameUpdate(game, cell, player);
       }
-      if (cell.hasContent && game.turn === "player2" && isPlayer2) {
+      if (!cell.hasContent && game.turn === "player2" && isPlayer2) {
         gameUpdate(game, cell, player);
       }
     }
@@ -23,19 +29,16 @@ const TicTacToe = () => {
       {game.board.length > 1 &&
         game.board.map((cell) => (
           <button
+            disabled={gameResult.result}
             key={cell.uid}
             onClick={() => checkLegalMove(cell)}
             className={`cell x-${cell.x} y-${cell.y} ${
               isPlayer1 ? "player1" : ""
             }${isPlayer2 ? "player2" : ""}
             ${
-              cell.content && game.players.player1.uid === cell.content
-                ? "exes"
-                : ""
+              cell.content && game.player1.uid === cell.content ? "exes" : ""
             } ${
-              cell.content && game.players.player2.uid === cell.content
-                ? "circle"
-                : ""
+              cell.content && game.player2.uid === cell.content ? "circle" : ""
             }`}
           />
         ))}
