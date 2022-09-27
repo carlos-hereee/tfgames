295ce70cca6bb4b067a85e35e226aa221af16c5b
ch04937 <97hernandez.c@gmail.com>
2022-09-26 20:00:57 -0700

    updating style components

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,15 +1,13 @@
 // import { faPlusSquare } from "@fortawesome/free-solid-svg-icons";
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useContext } from "react";
-// import shortid from "shortid";
 import { AuthContext } from "../context/AuthContext";
-// import Frame from "../components/Frame";
 import PlayerCard from "../components/PlayerCard";
 
 const Dashbaord = ({ history }) => {
   const { player, logOut } = useContext(AuthContext);
   return (
-    <section className="dashboard">
+    <section className="dashboard m-auto">
       <PlayerCard data={player} />
       <button
         className="btn btn-danger"
